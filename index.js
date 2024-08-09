819d128808ff8e5645b03a2e0572f0c6483c9194
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-08-09 15:44:03 -0300

    feature: exactlyTime challenge

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+/* 
+    challenge: create query exactlyTime
+    in this query, convert newDate instance to string value, and return.
+*/
+
 const typeDefs = gql`
     # Pontos de entrada da sua API!
     type Query {
         ola: String
+        exactlyTime: String
     }
 `
 
@@ -11,6 +17,9 @@ const resolvers = {
     Query: {
         ola() {
             return 'Retorno da query Ola'
+        },
+        exactlyTime() {
+            return new Date().toString();
         }
     }
 }
