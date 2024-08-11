7b2da0a1ec3a71d5b92cfdffafbfe336473585cb
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2024-08-11 10:30:49 -0300

    feature: type user

--- index.js
+++ index.js
@@ -3,20 +3,46 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
     scalar Date
 
+    type User {
+        # ! = required
+        id: ID
+        name: String!
+        email: String!
+        age: Int
+        salary: Float
+        vip: Boolean
+    }
+
     # Pontos de entrada da sua API!
     type Query {
-        ola: String
-        exactlyTime: Date
+        ola: String!
+        exactlyTime: Date!
+        user: User
     }
 `
 
 const resolvers = {
+    User: {
+        salary(payload) {
+            return payload.real_salary;
+        }
+    },
     Query: {
         ola() {
             return 'Retorno da query Ola'
         },
         exactlyTime() {
             return new Date;
+        },
+        user() {
+            return {
+                id: 1,
+                name: "Augusto",
+                email: "teste@email.com",
+                age: 10,
+                real_salary: 123.456,
+                vip: true
+            }
         }
     }
 }
